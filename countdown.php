a80743d6d55baac12f5aab517f3b7a276e933ecd
M0TZO <gadget17@gmail.com>
2013-12-19 01:31:14 +0000

    Update countdown.php

--- countdown.php
+++ countdown.php
@@ -1,15 +1,35 @@
+<html>
+<head>
+<title>KML countdown</title>
+<meta http-equiv="refresh" content="32">
+<meta name="viewport" content="width=device-width">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<style type="text/css" media="screen">
+   body {
+     background-color:#000;
+     color:#00CD00;
+     font-family: Tahoma, Geneva, sans-serif;
+     font-size:1.6em;
+   }
+  </style>
+
+</head>
+<body>
 <?php
 $urltoRichmond = "http://countdown.tfl.gov.uk/stopBoard/51750";
-$urltoKingston  = "http://countdown.tfl.gov.uk/stopBoard/56822"
+$urltoKingston  = "http://countdown.tfl.gov.uk/stopBoard/56822";
 
 foreach (array($urltoRichmond,$urltoKingston) as $url) {
 
   $result= file_get_contents($url);
   $info =  (json_decode($result,true));
-
+	print("<br/>Last updated: " . $info['lastUpdated'] . ".\n<br/>" );
   foreach ($info['arrivals'] as $bus ) {
-		print ("Next " . $bus['routeName'] . " bus to " . $bus['destination'] . "  " . $bus['estimatedWait'] . ".\n"  );
+		print ("" . $bus['routeName'] . " to " . 
+		$bus['destination'] . "  " . $bus['estimatedWait'] . ".\n<br/>"  );
   }
 }
 
 ?>
+</body>
+</html>

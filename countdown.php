90ff05ff4b11982abddf76fbcadc63bcc960d033
Andres Muniz-Piniella <a75576@alumni.tecnun.es>
2013-12-22 21:53:33 +0000

    some HTML5 formating I looked up. code copy and paste, added mariner gardens as it is also nearby

--- countdown.php
+++ countdown.php
@@ -22,8 +22,10 @@ please contribuite! This software is released under AGPL 3 or later
 see https://www.gnu.org/licenses/agpl-3.0.html for more details.
 -->
 <header>
-Buses near our makerspace
+<h4>Buses near our makerspace</h4>
 </header>
+<section>
+<h5>Buses on Ashburnham Road</h5>
 <?php
 $urltoRichmond = "http://countdown.tfl.gov.uk/stopBoard/51750";
 $urltoKingston  = "http://countdown.tfl.gov.uk/stopBoard/56822";
@@ -32,7 +34,7 @@ foreach (array($urltoRichmond,$urltoKingston) as $url) {
 
   $result= file_get_contents($url);
   $info =  (json_decode($result,true));
-	print("\nLast updated " . $info['lastUpdated'] . "\nFrom Ashburnham Road\n" );
+	//print("\nLast updated " . $info['lastUpdated'] . "\n");
   $line = 0;
   foreach ($info['arrivals'] as $bus ) {
   		++$line;
@@ -41,5 +43,27 @@ foreach (array($urltoRichmond,$urltoKingston) as $url) {
 }
 echo("</pre>");
 ?>
+</section>
+
+<section>
+<h5>Buses on Mariner Gardens</h5>
+<?php
+$urltoRichmond = "http://countdown.tfl.gov.uk/stopBoard/57780";
+$urltoKingston  = "http://countdown.tfl.gov.uk/stopBoard/57522";
+echo("<pre>");
+foreach (array($urltoRichmond,$urltoKingston) as $url) {
+
+  $result= file_get_contents($url);
+  $info =  (json_decode($result,true));
+	//print("\nLast updated " . $info['lastUpdated'] . "\nFrom Mariner Gardens\n" );
+  $line = 0;
+  foreach ($info['arrivals'] as $bus ) {
+  		++$line;
+  		printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
+  }
+}
+echo("</pre>");
+?>
+
 </body>
 </html>

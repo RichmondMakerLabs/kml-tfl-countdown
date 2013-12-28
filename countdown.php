4b5b6e080304cb65c37ac0474db97b3eace28030
M0TZO <gadget17@gmail.com>
2013-12-28 01:45:19 +0000

    Andrés Changes
    Took some tweaking to get working, but I've added the changes for the files Andrés made.
    
    The ico files work on Chrome and on the iPhone Chrome version.
    
    Andrés:- Changes may have failed as I had already updated the master so your version was already different.

--- countdown.php
+++ countdown.php
@@ -1,17 +1,21 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Buses from Kingston MakerLabs</title>
-<meta http-equiv="refresh" content="16">
-<meta name="viewport" content="width=device-width">
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<style type="text/css" media="screen">
-   body {
-     background-color:#000;
-     color:#FFA500;
-     font-family: Tahoma, Geneva, sans-serif;
-     font-size:1.2em;
-   }
-  </style>
+    <meta charset="utf-8" />
+    <title>Buses from Kingston Makerlabs</title>
+    <link rel="icon" type="image/png" href="http://www.kml.dreamhosters.com/bus/Bus_stop_ico.png">
+    <link rel="apple-touch-icon" href="/Bus_stop.png"/>
+    <meta http-equiv="refresh" content="16">
+    <meta name="viewport" content="width=device-width">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <style type="text/css" media="screen">
+       body {
+         background-color:#000;
+         color:#FFA500;
+         font-family: Tahoma, Geneva, sans-serif;
+         font-size:1.2em;
+       }
+      </style>
 </head>
 <body>
 <!-- insert comment here -->
@@ -32,11 +36,11 @@ foreach (array($urltoAlpha) as $url) {
 
   $result= file_get_contents($url);
   $info =  (json_decode($result,true));
-	//print("\nLast updated " . $info['lastUpdated'] . "\n");
+        //print("\nLast updated " . $info['lastUpdated'] . "\n");
   $line = 0;
   foreach ($info['arrivals'] as $bus ) {
-  		++$line;
-  		printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
+                  ++$line;
+                  printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
   }
 }
 echo("</pre>");
@@ -51,11 +55,11 @@ foreach (array($urltoBravo) as $url) {
 
   $result= file_get_contents($url);
   $info =  (json_decode($result,true));
-	//print("\nLast updated " . $info['lastUpdated'] . "\n");
+        //print("\nLast updated " . $info['lastUpdated'] . "\n");
   $line = 0;
   foreach ($info['arrivals'] as $bus ) {
-  		++$line;
-  		printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
+                  ++$line;
+                  printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
   }
 }
 echo("</pre>");
@@ -70,11 +74,11 @@ foreach (array($urltoCharlie) as $url) {
 
   $result= file_get_contents($url);
   $info =  (json_decode($result,true));
-	//print("\nLast updated " . $info['lastUpdated'] . "\nFrom Mariner Gardens\n" );
+        //print("\nLast updated " . $info['lastUpdated'] . "\nFrom Mariner Gardens\n" );
   $line = 0;
   foreach ($info['arrivals'] as $bus ) {
-  		++$line;
-  		printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
+                  ++$line;
+                  printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
   }
 }
 echo("</pre>");
@@ -89,11 +93,11 @@ foreach (array($urltoDelta) as $url) {
 
   $result= file_get_contents($url);
   $info =  (json_decode($result,true));
-	//print("\nLast updated " . $info['lastUpdated'] . "\nFrom Mariner Gardens\n" );
+        //print("\nLast updated " . $info['lastUpdated'] . "\nFrom Mariner Gardens\n" );
   $line = 0;
   foreach ($info['arrivals'] as $bus ) {
-  		++$line;
-  		printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
+                  ++$line;
+                  printf("%-2d %-4s %-16s%2d min\n",$line,$bus['routeName'],$bus['destination'],$bus['estimatedWait']);
   }
 }
 echo("</pre>");
